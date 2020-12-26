a71c65f2ed056ffef1c816a05b0a615fa3ad301e
ZouhourBerraies <76598580+zouhour18@users.noreply.github.com>
2020-12-26 21:29:16 +0100

    problem3.py

--- problem3.py
+++ problem3.py
@@ -1,1 +1,19 @@
 
+ch=input()
+s=""
+j=0
+for i in range (len(ch)):
+    if ch[i]=='A':
+        s[j]=ch[i]
+        j+=1
+    elif ch[i]=='Z':
+        for k in range (25):
+            s[j]=' '
+            j+=1
+        s[j]=ch[i]
+        j+=1
+    else :
+        s[j]=' '
+        s[j+1]=ch[i]
+        j+=2
+print(s)     
