598ede43137787337dd7b4b33656f9144f97f337
ZouhourBerraies <76598580+zouhour18@users.noreply.github.com>
2020-12-26 20:13:45 +0100

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,6 +1,6 @@
-def degpow(n,p)
-s=0
+def degpow(n,p) :
 ch=str(n)
+s=0
 for i in range (len(ch))
    s+=int(ch[i])**p
    p+=1
@@ -9,5 +9,13 @@ if type(k)==int
    return k
 else 
    return -1
-
-   
+while True
+  n=int(input("donner un entier positive "))
+  if n>0
+    return n
+while True
+  p=int(input("donner un entier positive "))
+  if p>0
+    return p
+k=degpow(n,p)
+print (k)
