2e30bf0c0dd329f7bf724bec323ebd8c0beae321
ZouhourBerraies <76598580+zouhour18@users.noreply.github.com>
2020-12-26 21:10:13 +0100

    probleme6.py

--- problem6.py
+++ problem6.py
@@ -1,1 +1,13 @@
-
+while True :
+  n=int (input("donner un entier"))
+  if (n>=0)and (n<10000) :
+    break 
+if n!=0:
+ for i in range (n):
+    ch+='('
+ for i in range (n):
+    ch+=')'
+else :
+    ch='.'
+print (ch)    
+    
