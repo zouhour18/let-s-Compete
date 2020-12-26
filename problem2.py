0035a3103d437b1d3b7bf316627e4bcee9932079
ZouhourBerraies <76598580+zouhour18@users.noreply.github.com>
2020-12-26 20:39:42 +0100

    problem2.py

--- problem2.py
+++ problem2.py
@@ -1,1 +1,19 @@
+def toBinary (n):
+  ch=""
+  m=0
+  while (n!=0):
+    m=n%2
+    ch=str(m)
+    n=n//2
+  b=""  
+  for i in range (len(ch)) : 
+      b[i]=ch[len(ch)-i-1]
+  return b
+while True :
+   n=int(input("donner un entier non_negative  "))
+   if n>0 :
+    break
+ch=toBinary(n)
+print (n,"en binaire = ",ch)
 
+   
